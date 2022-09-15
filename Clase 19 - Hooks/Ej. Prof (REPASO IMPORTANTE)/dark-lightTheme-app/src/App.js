46ad59d9f2066dbd56b2115f0c153acb36e26218
emelymack <emelymack@gmail.com>
2022-09-15 16:24:21 -0300

    Added class 19 - Hooks

---
+++ Clase 19 - Hooks/Ej. Prof (REPASO IMPORTANTE)/dark-lightTheme-app/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './styles.css';
+import Layout from './components/Layout';
+import ThemeContext, { themes } from './context';
+
+export default function App() {
+  const [theme, setTheme] = useState(themes.light);
+
+  const handleChangeTheme = () => {
+    // console.log(theme);
+    setTheme(() => {
+      return theme === themes.dark ? themes.light : themes.dark;
+    });
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, handleChangeTheme }}>
+      <Layout>
+        <div className="App">
+          <h1>React Context</h1>
+        </div>
+      </Layout>
+    </ThemeContext.Provider>
+  );
+}
